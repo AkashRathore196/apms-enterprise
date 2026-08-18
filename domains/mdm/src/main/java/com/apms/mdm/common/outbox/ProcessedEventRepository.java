ac0148e03fe9e93482e77b0797c12eb452b2408d
AkashRathore196 <63780270+AkashRathore196@users.noreply.github.com>
2026-08-19 00:26:58 +0530

    feat(mdm): add processed-event repository

---
+++ domains/mdm/src/main/java/com/apms/mdm/common/outbox/ProcessedEventRepository.java
@@ -0,0 +1,5 @@
+package com.apms.mdm.common.outbox;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProcessedEventRepository extends JpaRepository<ProcessedEvent, String> {}
