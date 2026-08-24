d626ca7329f275b6b9714dbbb3542c27ce005999
AkashRathore196 <63780270+AkashRathore196@users.noreply.github.com>
2026-08-24 20:06:10 +0530

    test(mdm): provide security resource-server properties for integration context

--- domains/mdm/src/test/java/com/apms/mdm/party/PartyServiceIntegrationTest.java
+++ domains/mdm/src/test/java/com/apms/mdm/party/PartyServiceIntegrationTest.java
@@ -3,6 +3,7 @@ package com.apms.mdm.party;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @Testcontainers
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PartyServiceIntegrationTest {
 
@@ -24,6 +26,9 @@ class PartyServiceIntegrationTest {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri", () -> "http://localhost:18080/realms/apms");
+        registry.add("spring.security.oauth2.resourceserver.jwt.jwk-set-uri", () -> "http://localhost:18080/realms/apms/protocol/openid-connect/certs");
+        registry.add("spring.security.oauth2.resourceserver.jwt.jws-algorithms", () -> "RS256");
     }
 
     @Test
